73e8382133ffd8f2c27715350338493fb99b6b42
ceeya12 <62365594+ceeya12@users.noreply.github.com>
2024-01-18 21:52:43 -0600

    2nd Big Commit

--- script/script.js
+++ script/script.js
@@ -1,16 +1,18 @@
 "use strict";
 
-/////////////////////////////////////////////////////////////////
-//Implementing full text search components
-//Search Implementation
-
-// const searchFieldForm = document.getElementById("searchField");
-// const searchBtn = document.getElementById("searchBtn");
 const closeModalBtn = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal_custom");
+const searchForm = document.querySelector("#search-form");
+const searchResults = document.querySelector("#search-results");
+//images dom entry point for lazy loading images
+const imgTargets = document.querySelectorAll("img[data-src]");
 
-//Function for Opening Modal Window
+/////////////////////////////////////////////////////////////////
+//Implementing full text search components
+//Search Implementation
+//Function for Opening Modal Window when search input entered
+//Full Text Search
 
 const openModal = function () {
   modal.classList.remove("hidden_all");
@@ -23,9 +25,20 @@ const closeModal = function () {
   overlay.classList.add("hidden_all");
 };
 
+const disableScroll = function () {
+  document.body.classList.add("remove-scrolling");
+};
+const enableScroll = function () {
+  document.body.classList.remove("remove-scrolling");
+};
+
+//Full Text Search
+//function that fetches json data
+//locally stored json file
+
 const getSearchData = async function () {
   try {
-    const response = await fetch("/san_marcos_orthopedics_search_index.json");
+    const response = await fetch("search_index.json");
 
     console.log(response.status);
 
@@ -40,39 +53,50 @@ const getSearchData = async function () {
   }
 };
 
-async function retrieveSearchResults(query) {
-  const posts = await getSearchData();
+//Full Text Search
+//function to call Fuse.js API
+//keys are passed for search parameters
 
-  // const fuse = new Fuse(posts);
-  const fuse = new Fuse(posts, {
-    keys: ["surgeons.specialities.value"],
-  });
+async function retrieveSearchResults(query) {
+  //posts = san_marcos_orthopedics_search_index.json
+  const posts = await getSearchData();
+  const options = {
+    keys: ["specialities.tags"],
+    // keys: [["surgeons", "firstname", "lastname", "specialities", "value"]],
+  };
+
+  const fuse = new Fuse(posts, options);
 
   const searchResults = fuse.search(query);
 
   return searchResults;
 }
 
+//Full Text Search
+// function to display HTML info on modal window
+// with respective doctor
+//<img src="/doctor_headshot_images/jd_robinson_thumbnail.png" alt="thumbnail surgeon"></img>
+
 function generatePostHTML(post) {
   const renderHTML = `
-  <p>${post.item.title}</p>
-  <p>${post.item.phonenumber}</p>
-  <p><a href="/johndrobinson.html">${post.item.surgeons.firstname} ${post.item.surgeons.lastname}</a></p>
-  
+  <p><a href="${post.item.href}">${post.item.surgeon}</a></p>
+  <p class="center_search_content">${post.item.phonenumber}</p>
+  <a href="online_form_appointment.html">
+  <p class="center_search_content">${post.item.email}</p>
+  </a>
   `;
+
   return renderHTML;
 }
 
+//Full text search
 //Disable Scrolling during search input from
-
 //event listeners
 
-const searchForm = document.querySelector("#search-form");
-const searchResults = document.querySelector("#search-results");
-
-searchForm.addEventListener("submit", async function (e) {
+searchForm.addEventListener("input", async function (e) {
   e.preventDefault();
   openModal();
+  disableScroll();
 
   const formData = new FormData(searchForm);
   const query = formData.get("search");
@@ -92,23 +116,196 @@ searchForm.addEventListener("submit", async function (e) {
 //modal button X
 closeModalBtn.addEventListener("click", function () {
   closeModal();
+  enableScroll();
 });
 //close modal event handler
 //esc key and backspace
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden_all")) {
     closeModal();
+    enableScroll();
   }
 
   if (e.key === "Backspace" && !modal.classList.contains("hidden_all")) {
     closeModal();
+    enableScroll();
   }
 });
 
+/////////////////////////////////////////////////////////////////////////////
+//popovers over Nav links
+//path is Services -> Joint Replacement...Sports Medicine
+
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const element = document.getElementById("myPopover");
+  const htmlContent = `
+                    <div class ="popover_nav">
+                    <h6 class="popover_title">Surgeons</h6>
+                    <ul class="popover_title">
+                    <img src="/doctor_headshot_images/joshua_crum_thumbnail_circular.png" alt="">
+                    <h6>Dr. Crum</h6>
+
+                    <img src="/doctor_headshot_images/jd_robinson_thumbnail_circular.png" alt="">
+                    <h6>Dr. Robinson</h6>
+
+                    </ul>
+                  </div>
+
+  
+  `;
+  const popover = new bootstrap.Popover(element, {
+    // title:
+    //   '<h6 class="custom-title"><i class="bi-info-circle-fill"></i> Popover info</h6>',
+    // content: '<p class="popover_nav">Test</p>',
+    content: htmlContent,
+    html: true,
+  });
+});
+
+//////////////////////////////////////////////////////////////////////////////
+//lazy loading functionality for images
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(imgTargets);
+
+  if (!entry.isIntersecting) return;
+
+  //replace the src attribute from image with data-src
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-loading-img");
+  });
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+/////////////////////////////////////////////////////////////////////////////
+//popovers over headshots
+//headshot for surgeon -> popover
+const popOverMD = document.getElementById("headShotMD");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const html = `<div class="popover_container">
+                <ul> 
+                <li class="animation_li">Foot and ankle surgery</li>
+                <li class="animation_li">Injections, ingrown nails, wound care, diabetic care </li>
+                <li class="animation_li">Bunions, hammertoes, ankle arthroscopy, arthrities, heal pain</li>
+                <li class="animation_li">Ankle sprains, total ankle replacement, fractures of the foot/ankle</li>
+                </ul>
+                </div>`;
+
+  const popover = new bootstrap.Popover(popOverMD, {
+    title: '<h6 class="popover_title">Top Procedures</h6>',
+    content: html,
+    html: true,
+  });
+});
+
+const popOverJC = document.getElementById("popOverJC");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const html = `<div class="popover_container">
+                <ul> 
+                <li class="animation_li">Sports Medicine</li>
+                <li class="animation_li">General Orthopedics</li>
+                <li class="animation_li">Knee and Shoulder Arthroscopy</li>
+                <li class="animation_li">Shoulder, hip, and knee joint replacement</li>
+                <li class="animation_li">Fracture Care</li>
+                <li class="animation_li">Injections</li>
+                </ul>
+                </div>`;
+
+  const popover = new bootstrap.Popover(popOverJC, {
+    title: '<h6 class="popover_title">Top Procedures</h6>',
+    content: html,
+    html: true,
+  });
+});
+
+const popOverGm = document.getElementById("popOverGm");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const html = `<div class="popover_container">
+                <ul> 
+                <li class="animation_li">Hand and wrist surgery</li>
+                <li class="animation_li">Fractures and trauma of the hand and wrist</li>
+                <li class="animation_li">Degenerative conditions</li>
+                <li class="animation_li">Neuropathies</li>
+                <li class="animation_li">Microsurgery</li>
+                </ul>`;
+
+  const popover = new bootstrap.Popover(popOverGm, {
+    title: '<h6 class="popover_title">Top Procedures</h6>',
+    content: html,
+    html: true,
+  });
+});
+
+const popOverJr = document.getElementById("popOverJr");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const html = `
+              <div class="popover_container">
+                <ul> 
+                <li class="animation_li">Sports Medicine</li>
+                <li class="animation_li">Knee and Shoulder Arthroscopy</li>
+                <li class="animation_li">General Orthopedics</li>
+                <li class="animation_li">Patient-Specific shoulder replacement</li>
+                <li class="animation_li">Mako robtic hip and knee joint replacement</li>
+                <li class="animation_li">Fracture Care</li>
+                <li class="animation_li">Injections</li>
+                </ul>
+                </div>
+                `;
+
+  const popover = new bootstrap.Popover(popOverJr, {
+    title: '<h6 class="popover_title">Top Procedures</h6>',
+    content: html,
+    html: true,
+  });
+});
+
+const popOverAs = document.getElementById("popOverAs");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const html = `<div class="popover_container">
+                <ul>
+                <li class="animation_li">Foot and ankle Surgery</li>
+                <li class="animation_li">Injections, ingrown nails, would care, diabetic care </li>
+                <li class="animation_li">Bunions, hammertoes, ankle arthroscopy, arthrities, heal pain</li>
+                <li class="animation_li">Ankle sprains, total ankle replacement, fractures of the foot/ankle</li>
+                </ul>
+                </div>`;
+
+  const popover = new bootstrap.Popover(popOverAs, {
+    title: '<h6 class="popover_title">Top Procedures</h6>',
+    content: html,
+    html: true,
+  });
+});
+
+////////////////////////////////////////////////////////////////////////
 /*Navy blue 3rd conatiner on index page */
+//Display on screen is :
+// 5 orthopedics Specialists 12 Years in practice
 
 const counters = document.querySelectorAll(".counter");
-const containerDoctorCount = document.querySelector("section");
+const containerDoctorCount = document.getElementById("counterBox");
 
 containerDoctorCount.addEventListener("mouseenter", function () {
   //loop to iterate through each number and display value
@@ -167,7 +364,7 @@ containerDoctorCount.addEventListener("mouseenter", function () {
   d[l]
     ? console.warn(p + " only loads once. Ignoring:", g)
     : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
-})({ key: "AIzaSyD7dJQe310hYqb1SXhwttNhjsKay8cDkao", v: "beta" });
+})({ key: "", v: "beta" });
 
 // Initialize and add the map
 let map;
@@ -196,3 +393,23 @@ async function initMap() {
 }
 
 initMap();
+
+///////////////////////////////////////////////////////////////
+//Appointment form request submission
+//appointment.html
+
+// import emailjs from "@emailjs/browser";
+
+//const form = document.getElementById("appointmentForm");
+// const firstNameInput = document.getElementById("firstName");
+// const lastNameInput = document.getElementById("lastName");
+// const mobileNumber = document.getElementById("mobileNumber");
+// const email = document.getElementById("email");
+
+// firstNameInput.addEventListener("input", (e) => console.log(e.target.value));
+
+//validation needed for form
+
+// mobileNumber.addEventListener("input", function (e) {
+//   console.log(e.target.value);
+// });
