97604ee35b22dfd5840682693f152c979cfd1c80
ceeya12 <62365594+ceeya12@users.noreply.github.com>
2024-01-22 12:19:33 -0600

    mark update

--- dist/script/script.js
+++ dist/script/script.js
@@ -216,7 +216,7 @@ const htmlMark = `<div class="popover_container">
 const popoverListHeadShotMark = [...popoverTriggerListHeadShotMark].map(
   (popoverTriggerEl) =>
     new bootstrap.Popover(popoverTriggerEl, {
-      placement: "bottom",
+      offset: [-20, 10],
       title: "<h6 class='d-flex justify-content-center'>TOP PROCEDURES</h6>",
       content: htmlMark,
       html: true,
