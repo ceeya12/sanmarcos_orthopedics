68e12c15018d9c915f5b6abb644c5d16ac3574d5
ceeya12 <62365594+ceeya12@users.noreply.github.com>
2024-01-28 12:05:31 -0600

    commit change to George picure adjustment

--- dist/script/script.js
+++ dist/script/script.js
@@ -266,7 +266,7 @@ const popoverTriggerListHand = document.querySelectorAll(
 
 const htmlContentHand = `
 <div class="row">
-  <div class="col">
+  <div class="col d-flex justify-content-center">
   <h6 class="popover_surgeon_name">Dr. Mundanthenum</h6>
     <img class="popover_nav_img_spacing"
       src="images/doctor_headshot_images/gm_thumbnail_circular.png"
